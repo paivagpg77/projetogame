3708c5495da2f79fe09bfa7de464212fdd467726
paivagpg77 <paivag171@gmail.com>
2025-10-27 13:06:44 -0300

    Código do Jogo

---
+++ flappybird.py
@@ -0,0 +1,131 @@
+import pygame
+import os
+import random
+
+TELA_LARGULA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
+IMAGEM_CHAO =   pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
+IMAGEM_BACKGROUND =  pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
+IMAGENS_PASSARO =  [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png')))
+]
+pygame.font.init()
+FONTE_PONTOS =pygame.font.SysFont('arial',50)
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+    #Animações
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+    
+    def __init__(self , x ,y):
+        self.x = x 
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0 
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = self.IMGS[0]
+        
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+        # restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        # o angulo do passaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+        
+    def desenhar(self, tela):
+        self.contagem_imagem += 1
+        if  self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO *2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4+1:
+            self.imagem = self.IMGS[0]
+            self.imagem = 0 
+
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
+
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        posicao_centroimg = self.imagem.get_rect(topleft=(self.x, self.y)).center 
+        retangulo = imagem_rotacionada.get_rect(center =posicao_centroimg)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
+
+
+class Cano:
+    DISTANCIA = 200
+    VELOCIDADE = 5
+    
+    def __init__(self,x):
+        self.x = x
+        self.altura = 0
+        self.posica_topo = 0
+        self.posicao_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO,False ,True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False 
+        self.definir_altura()
+    
+    def definir_altura(self):
+        self.altura = random.randrange(50 , 450)
+        self.posicao_base = self.altura - self.CANO_TOPO.get_height()
+        self.posica_base = self.altura + self.DISTANCIA 
+    
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x ,self.posica_topo))
+        tela.blit(self.CANO_BASE, (self.x ,self.posica_base))
+    
+    def colidir(self,passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x , self.posica_topo  - round(passaro.y))
+        distancia_base = (self.x - passaro.x , self.posica_base  - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else: 
+            return False
+class Chao:
+    pass
